cfe8b39c6ed08ab9043c91dea87df167e42d4652
OshidhieP <oshidhiep@lolctech.com>
2024-03-08 10:53:09 +0530

    Customer Bank Accounts Cards completed

--- src/test/java/com/ipay/tests/TestClass.java
+++ src/test/java/com/ipay/tests/TestClass.java
@@ -1,10 +1,7 @@
 package com.ipay.tests;
 
-import com.ipay.main.pages.CustomerSearch;
-import com.ipay.main.pages.CustomerTransaction;
-import com.ipay.main.pages.Dashboard;
+import com.ipay.main.pages.*;
 import com.ipay.main.webdriver.DriverInitialization;
-import com.ipay.main.pages.LoginPage;
 import org.json.simple.JSONObject;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.*;
@@ -218,137 +215,33 @@ public class TestClass {
         CustomerTransaction.successfullyLoaded(driver);
     }
 
+    @Test(testName = "Customer bank accounts & cards using cards", priority = 6)
+    public void searchCustomerUsingBankAccNoUsingCards() throws InterruptedException {
+        CustomerBankAccountsCards.clickCustomer(driver);
+        CustomerBankAccountsCards.clickCustomerBankAcc(driver);
+        CustomerBankAccountsCards.search(driver);
+        CustomerBankAccountsCards.enterNic(driver, cusSearchTestData.get("nic").toString());
+        CustomerBankAccountsCards.clickSearch(driver);
+        CustomerBankAccountsCards.clickElements(driver);
+        CustomerBankAccountsCards.clickCards(driver);
+        CustomerBankAccountsCards.clickDelete(driver);
 
+    }
 
-//    @Test(testName = "Customer bank accounts & cards", priority = 6)
-//    public void searchCustomerUsingBankAccNoUsingCards() throws InterruptedException {
-//        CustomerBankAccountsCards.clickCustomer(driver);
-//        CustomerBankAccountsCards.clickCustomerBankAcc(driver);
-//        CustomerBankAccountsCards.search(driver);
-//        CustomerBankAccountsCards.enterNic(driver, cusSearchTestData.get("nic").toString());
-//        CustomerBankAccountsCards.clickSearch(driver);
-//        CustomerBankAccountsCards.clickElements(driver);
-//    //    Thread.sleep(50000);
-//        CustomerBankAccountsCards.clickCards(driver);
-//        CustomerBankAccountsCards.clickDelete(driver);
-//        CustomerBankAccountsCards.successfullyDeleted(driver); //Assertion
-//    }
+    @Test(testName = "Customer bank accounts & cards using bank accounts", priority = 6)
+    public void searchCustomerUsingBankAccNoUsingBankAcc() throws InterruptedException {
+        CustomerBankAccountsCards.clickCustomer(driver);
+        CustomerBankAccountsCards.clickCustomerBankAcc(driver);
+        CustomerBankAccountsCards.search(driver);
+        CustomerBankAccountsCards.enterNic(driver, cusSearchTestData.get("nic").toString());
+        CustomerBankAccountsCards.clickSearch(driver);
+        CustomerBankAccountsCards.clickElements(driver);
+        CustomerBankAccountsCards.clickBank(driver);
+        CustomerBankAccountsCards.clickBankDelete(driver);
+    }
 
-
-//    @Test(testName = "Verify the response for merchant Customer registration using valid data", priority = 1)
-//    public void merchantRegistration() throws InterruptedException {
-//        MerchantRegistration.clickMerchant(driver);
-//        MerchantRegistration.clickMerchantReg(driver);
-//        MerchantRegistration.clickBType(driver);
-//        MerchantRegistration.clickType(driver);
-//        MerchantRegistration.clickSelect(driver);
-//        MerchantRegistration.empNoSearch(driver, testData.get("validEmpNo").toString());
-//        MerchantRegistration.clickEmpNoSearch(driver);
-//        MerchantRegistration.clickNext(driver);
-//        MerchantRegistration.enterMerchantName(driver, testData.get("merchantName").toString());
-//        MerchantRegistration.enterBCode(driver, testData.get("businessCode").toString());
-//        MerchantRegistration.bCategory(driver, testData.get("businessCategory").toString());
-//        MerchantRegistration.next1(driver);
-//        MerchantRegistration.enterNIC(driver, testData.get("nicNumber").toString());
-//        String imagePath = new File("src/main/resources/images/nic.jpg").getAbsolutePath();
-//        MerchantRegistration.uploadNic(driver, imagePath);
-//        MerchantRegistration.addressLine1(driver, testData.get("addressLine1").toString());
-//        MerchantRegistration.addressLine2(driver, testData.get("addressLine2").toString());
-//        MerchantRegistration.cityEnter(driver, testData.get("city").toString());
-//        Thread.sleep(3000);
-//        MerchantRegistration.next2(driver);
-//        Thread.sleep(2000);
-//        MerchantRegistration.mUsername(driver, testData.get("merchantUsername").toString());
-//        MerchantRegistration.mMobileNo(driver, testData.get("merchantMobileNo").toString());
-//        Thread.sleep(3000);
-//        MerchantRegistration.next3(driver);
-//        MerchantRegistration.bank(driver, testData.get("bankName").toString());
-//        MerchantRegistration.accNo(driver, testData.get("accountNumber").toString());
-//        MerchantRegistration.finish(driver);
-//        MerchantRegistration.invalidMessage(driver);//Assertion
-//    }
-
-
-
-//    @Test(testName = "Verify the response for merchant Customer registration without entering mandatory field", priority = 3)
-//    public void merchantRegistrationNegative() {
-//        MerchantRegistration.clickMerchant(driver);
-//        MerchantRegistration.clickMerchantReg(driver);
-//        MerchantRegistration.clickNext(driver);
-//        MerchantRegistration.isErrorMessageDisplayed(driver);
-//    }
-
-//    @Test(testName = "Invalid data enter", priority = 4)
-//    public void merchantNameInvalidInRegistration() throws InterruptedException {
-//        MerchantRegistration.clickMerchant(driver);
-//        MerchantRegistration.clickMerchantReg(driver);
-//        MerchantRegistration.clickBType(driver);
-//        MerchantRegistration.clickType(driver);
-//        MerchantRegistration.clickSelect(driver);
-//        MerchantRegistration.empNoSearch(driver, testData.get("invalidEmpNo").toString());
-//        Thread.sleep(2000);
-//        MerchantRegistration.errorMessageInIntroducer(driver);
-//    }
-//
-//    @Test(testName = "Check Invalid NIC no", priority = 5)
-//    public void invalidNicNo() throws InterruptedException {
-//        MerchantRegistration.clickMerchant(driver);
-//        MerchantRegistration.clickMerchantReg(driver);
-//        MerchantRegistration.clickBType(driver);
-//        MerchantRegistration.clickType(driver);
-//        MerchantRegistration.clickSelect(driver);
-//        MerchantRegistration.empNoSearch(driver, testData.get("validEmpNo").toString());
-//        MerchantRegistration.clickEmpNoSearch(driver);
-//        MerchantRegistration.clickNext(driver);
-//        MerchantRegistration.enterMerchantName(driver, testData.get("merchantName").toString());
-//        MerchantRegistration.enterBCode(driver, testData.get("businessCode").toString());
-//        MerchantRegistration.bCategory(driver, testData.get("businessCategory").toString());
-//        MerchantRegistration.next1(driver);
-//        MerchantRegistration.enterNIC(driver, testData.get("invalidNicNumber").toString());
-//        String imagePath = new File("src/main/resources/images/nic.jpg").getAbsolutePath();
-//        MerchantRegistration.uploadNic(driver, imagePath);
-//        MerchantRegistration.addressLine1(driver, testData.get("addressLine1").toString());
-//        MerchantRegistration.addressLine2(driver, testData.get("addressLine2").toString());
-//        MerchantRegistration.cityEnter(driver, testData.get("city").toString());
-//        Thread.sleep(3000);
-//        MerchantRegistration.invalidNIC(driver); //Assertion
-//    }
-//
-//    @Test(testName = "Check Invalid Merchant Mobile No", priority = 6)
-//    public void invalidMerchantMobileNo() throws InterruptedException {
-//        MerchantRegistration.clickMerchant(driver);
-//        MerchantRegistration.clickMerchantReg(driver);
-//        MerchantRegistration.clickBType(driver);
-//        MerchantRegistration.clickType(driver);
-//        MerchantRegistration.clickSelect(driver);
-//        MerchantRegistration.empNoSearch(driver, testData.get("validEmpNo").toString());
-//        MerchantRegistration.clickEmpNoSearch(driver);
-//        Thread.sleep(3000);
-//        MerchantRegistration.clickNext(driver);
-//        MerchantRegistration.enterMerchantName(driver, testData.get("merchantName").toString());
-//        MerchantRegistration.enterBCode(driver, testData.get("businessCode").toString());
-//        MerchantRegistration.bCategory(driver, testData.get("businessCategory").toString());
-//        MerchantRegistration.next1(driver);
-//        MerchantRegistration.enterNIC(driver, testData.get("nicNumber").toString());
-//        String imagePath = new File("src/main/resources/images/nic.jpg").getAbsolutePath();
-//        MerchantRegistration.uploadNic(driver, imagePath);
-//        MerchantRegistration.addressLine1(driver, testData.get("addressLine1").toString());
-//        MerchantRegistration.addressLine2(driver, testData.get("addressLine2").toString());
-//        MerchantRegistration.cityEnter(driver, testData.get("city").toString());
-//        Thread.sleep(3000);
-//        MerchantRegistration.next2(driver);
-//        Thread.sleep(2000);
-//        MerchantRegistration.mUsername(driver, testData.get("merchantUsername").toString());
-//        MerchantRegistration.mMobileNo(driver, testData.get("invalidMobileNo").toString());
-//        MerchantRegistration.next3(driver);
-//        Thread.sleep(3000);
-//        MerchantRegistration.invalidMerchantMobileNo(driver); //Assertion
-//    }
-
-//    @AfterMethod
-//    public void tearDown() {
-//        driver.quit();
-//    }
-
-
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 }
